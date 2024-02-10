f2fe215a200140631e4dafbd39ac7c42c507ec63
ahmet-yasir <ahmetyasirkulaksiz@gmail.com>
2024-02-10 16:35:28 -0500

    ex04

---
+++ ex04/ft_strstr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akulaksi <akulaksi@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/10 16:24:18 by akulaksi          #+#    #+#             */
+/*   Updated: 2024/02/10 16:35:14 by akulaksi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char    *ft_strstr(char *str, char *to_find)
+{
+    int i;
+    int j;
+    
+    i = 0;
+    if (*to_find == '\0')
+        return str;
+    while (str[i] != '\0')
+    {
+        j = 0;
+        while (to_find[j] != '\0' && str[i + j] == to_find[j])
+            j++;
+        if (to_find[j] == '\0')
+            return &str[i];
+        i++;
+    }
+    return (0);
+}
